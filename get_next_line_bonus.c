a2719a64b590c2922582484ae1a9439e5dac8050
Younghyo Cho <yhcho0405@kakao.com>
2021-01-02 16:16:21 +0900

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,6 +12,18 @@
 
 #include "get_next_line_bonus.h"
 
+size_t		ft_strlen(const char *str)
+{
+	size_t len;
+
+	if (!str)
+		return (FAIL);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
 int		make_line(char **line, char *buffer, int i)
 {
 	int ret;
