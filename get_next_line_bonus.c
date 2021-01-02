50b88a56b86d25480d46d849869d6da3a0806cd3
Younghyo Cho <yhcho0405@kakao.com>
2021-01-02 16:15:48 +0900

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 int		make_line(char **line, char *buffer, int i)
 {
